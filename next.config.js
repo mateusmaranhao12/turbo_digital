13d1a87824e7159fd0664de3c3f07080d9d7db66
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-03-27 15:10:33 -0300

    Update js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+  basePath: '/mentoria-turbo-digital',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
